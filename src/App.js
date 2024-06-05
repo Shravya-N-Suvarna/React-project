1207ee86fd8d31cb4e647e28332437be8b99460b
Shravya <shravyasuvarna1998@gmail.com>
2024-06-05 12:05:16 +0530

    changes commited

--- src/App.js
+++ src/App.js
@@ -11,9 +11,21 @@
 // export default App;
 import React from 'react'
 // import Test from './test';
- import Parent from './Parent';
- import Child from './Child';
+//  import Parent from './Parent';
+//  import Child from './Child';
+//map function
+// const cars = ["Ford", "BMW", "Audi"];
 function App() {
+  const handleChange = (event) =>{
+    console.log(event.target.value);
+  }
+  const handleSubmit = (event) =>{
+    event.preventDefault();
+  }
+  // const handleSubmit = (event) =>{
+  //   localStorage.setItem("name",name)
+  //   localStorage.getItem("name")
+  // }
   // const handleSubmit=(x)=>{
   //   alert(x)
   // let demo=false;
@@ -24,14 +36,25 @@ function App() {
   //     return <Child/>
       
   //   }
-    let bool=true;
-  
+    // let bool=true;
+  // let x=5;
   return (
      // <div className= "App"><Test/></div>
     // <div><Parent/></div>
     <div>
+      <form>
+        <label>Enter the Name</label>
+        <input type="text" name="name" onChange={handleChange}/>
+        <button type="submit" onClick={handleSubmit}>Submit</button>
+      </form>
+      {/* <ul>
+        {cars.map((car) => (
+          <li>{car}</li>
+        ))}
+      </ul> */}
        {/* <h1>{demo ?<Parent/> :<Child/>}</h1> */}
-       <h1>{bool ? <Parent/>:<Child/>}</h1>
+       {/* <h1>{bool ? <Parent/>:<Child/>}</h1> */}
+       {/* <h1>{x<=10 ? "true" : "false"}</h1> */}
     </div>
   )
 }
